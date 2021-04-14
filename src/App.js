d15826edcc5034e31eb467504a4fadf94df35b44
ed1868 <ruizeduardo21@gmail.com>
2021-04-14 14:26:27 -0400

    added items array ready to be looped

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/Accordion';
 
+const items = [{
+  title: "Why is React js the best?",
+  context: "because it is"
+},
+{
+  title: "Why is Eddie the best?",
+  context: "because he is"
+}, {
+  title: "Coin base IPO?",
+  context: "406"
+},
+
+]
+
 export default () => {
   return <div>
     <Accordion />
