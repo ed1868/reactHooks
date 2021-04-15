285481ed7f0fe39c9aff4259436b98fe3289af23
ed1868 <ruizeduardo21@gmail.com>
2021-04-15 09:57:11 -0400

    helper function and use state implemented

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,25 +1,30 @@
-import React from 'react';
-
-
+import React, { useState } from 'react';
 
 
 const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    const renderedItems = items.map((item,key) => {
+    const onTitleClick = (value) => {
+        setActiveIndex(value);
+    }
+
+
+    const renderedItems = items.map((item, key) => {
+
         return (
             <React.Fragment key={key}>
-                <div className="title active">
+                <div className="title active" onClick={() => onTitleClick(key)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
                 <div className="content active">
                     <p>{item.content}</p>
                 </div>
-        
+
             </React.Fragment>
         )
-        });
-    
+    });
+
     return <div className="ui styled accordion">{renderedItems}</div>
 }
 
