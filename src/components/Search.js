d3dd1f823b46097acd75bc98a804fcdcf7324b67
ed1868 <ruizeduardo21@gmail.com>
2021-04-18 23:38:44 -0400

    useEffect function placed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
+
 
 
 const Search = () => {
+
+    const [term, setTerm] = useState(null);
+
+
     return (
         <div>
-            <h1>Search: </h1>
+            <div className="ui form">
+                <div className="field">
+                    <label>Search Me: </label>
+                    <input value={term} onChange={(e) => setTerm(e.target.value)} className="input" />
+                </div>
+            </div>
         </div>
     )
 }
