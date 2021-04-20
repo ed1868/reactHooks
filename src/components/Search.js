61675b88747ffec854195534facc61473bd1fe4b
ed1868 <ruizeduardo21@gmail.com>
2021-04-20 15:44:48 -0400

    added use effect to make search queries both in video way and arrow function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 
 const Search = () => {
 
-    const [term, setTerm] = useState(null);
-
-    console.log('I run with any render');
+    const [term, setTerm] = useState(' ');
+    const [results, setResults] = useState([]);
+    console.log('RERENDERING...');
+    console.log(results);
 
     useEffect(() => {
-        console.log('I ONLY RUN ONCE');
-    }, [])
+        //ASYNC AWAIT INSTRUCTOR VERSION
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+
+                },
+
+
+            });
+            setResults(data);
+
+        }
+        if (term) {
+            search();
+        }
+
+
+
+
+        // ASYNC AWAIT WITH PROMISES (EDDIE FAV)
+        // const eddiesWay = () => {
+        //     axios.get('https://en.wikipedia.org/w/api.php', {
+        //         params: {
+        //             action: 'query',
+        //             list: 'search',
+        //             origin: '*',
+        //             format: 'json',
+        //             srsearch: term
+
+        //         },
+
+        //     }).then(payload => {
+        //         console.log(`EDDIES PAYLOAD : ${payload}`);
+
+        //         setResults(payload);
+
+        //     }).catch(err => {
+        //         if (err) {
+        //             console.log(`ERROR: ${err}`);
+        //         }
+        //     });
+
+        // }
+
+        // if (term) {
+        //     eddiesWay();
+        // }
+
+
+
+    }, [term]);
     return (
         <div>
             <div className="ui form">
